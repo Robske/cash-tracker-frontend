ff9c5d5334b554a18f9ff8b401e07917554524ba
Rob <robalma7@gmail.com>
2023-11-29 12:38:05 +0000

    x

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // api_url: 'https://vps.robalma.nl:5002/',
-  api_url: 'http://localhost:5003/',
+  api_url: 'https://vps.robalma.nl:5002/',
+  // api_url: 'http://localhost:5003/',
 };
